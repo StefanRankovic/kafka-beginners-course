f5bd18ef34a689efc5b4ea785bd38358708238e6
Stefan Rankovic <stefan.rankovic.89@gmail.com>
2020-02-16 18:30:46 +0100

    kafka connect and kafka streams

--- kafka-consumer-elastic-search/src/main/java/stefanrankovic/tutorial3/ElasticSearchConsumer.java
+++ kafka-consumer-elastic-search/src/main/java/stefanrankovic/tutorial3/ElasticSearchConsumer.java
@@ -1,8 +1,99 @@
 package stefanrankovic.tutorial3;
 
+import com.google.gson.JsonParser;
+import org.apache.http.HttpHost;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ElasticSearchConsumer {
+    private static JsonParser parser = new JsonParser();
 
-    public static void main(String[] args) {
+    private static RestHighLevelClient createClient() {
+        RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200, "http"));
 
+        return new RestHighLevelClient(builder);
+    }
+
+    private static KafkaConsumer<String, String> createConsumer() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, "kafka-demo-elasticsearch");
+        configs.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // disable autocommit
+        configs.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "100"); // disable autocommit
+
+        return new KafkaConsumer<>(configs);
+    }
+
+    private static String extractIdFromTweet(String value) {
+        return parser.parse(value).getAsJsonObject().get("id_str").getAsString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
+
+        RestHighLevelClient client = createClient();
+
+        KafkaConsumer<String, String> consumer = createConsumer();
+        consumer.subscribe(Collections.singletonList("twitter_tweets"));
+
+        while(true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            int count = records.count();
+            logger.info("Received {} records", count);
+
+
+            BulkRequest bulkRequest = new BulkRequest();
+
+
+            for (ConsumerRecord<String, String> record : records) {
+                try {
+                    String id = extractIdFromTweet(record.value());
+                    IndexRequest indexRequest = new IndexRequest("twitter", "tweets", id).source(record.value(), XContentType.JSON);
+                    bulkRequest.add(indexRequest);
+                } catch (NullPointerException e) {
+                    logger.info("Skipping bad data", e);
+                }
+
+            }
+
+            if (count > 0) {
+                BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+                logger.info("Committing the offsets...");
+                consumer.commitSync();
+
+                logger.info("Offsets have been committed");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+//        client.close();
     }
 }
